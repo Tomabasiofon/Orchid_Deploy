5f3bd69a4f0648801e7d9b287dd2272a96cf2026
Geniroh <irochibuzor@gmail.com>
2023-09-19 23:59:43 +0100

    image resize

--- backend/controllers/promo.controller.js
+++ backend/controllers/promo.controller.js
@@ -25,5 +25,32 @@ const getPromos = async (req,res,next) => {
         next(error)
     }
 }
-
-module.exports = { createPromo, getPromos }
+const deletePromo = async (req,res,next)=>{
+    const { code } = req.params
+    try {
+      if(code) {
+        const promo = await Promos.findOneAndDelete({code});
+        if(!promo) createError(404, "Cannot perform delete operation")    
+        res.status(200).json(promo);
+      } else {
+        createError(404, "Record was not found!")
+      }
+    } catch (err) {
+      next(err);
+    }
+  }
+  
+  const updatePromo = async (req,res,next)=>{
+    const { code } = req.params
+    try {
+      if(code) {
+        const promo = await Promos.findOneAndUpdate({ code }, {...req.body}, { new: true })
+        res.status(200).json(promo);
+      } else {
+        createError(500, "Error retrieving space record")
+      }
+    } catch (err) {
+      next(err);
+    }
+  }
+module.exports = { createPromo, getPromos, updatePromo, deletePromo }
