727b4bff25bf53c3eea678ff035a61a04e78d6f8
Geniroh <irochibuzor@gmail.com>
2023-10-09 06:39:38 +0100

    created cors list

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,10 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 const corsOptions ={
-    origin: '*', 
+    origin: [
+        'https://spaces.orchidsprings.group',
+        'http://admin.orchidsprings.group'
+    ], 
     credentials: true,
     optionsSuccessStatus: 200
 }
